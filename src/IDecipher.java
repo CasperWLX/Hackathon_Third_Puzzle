6cf75124822b3ce13c9a9fe52d3634e7099ebae7
Hampus Källberg <hampuskallberg@gmail.com>
2024-11-13 15:10:10 +0100

    Happy Coding!

---
+++ src/IDecipher.java
@@ -0,0 +1,9 @@
+/**
+ * Feel free to change any of these methods
+ * They are only here to give some basic direction.
+ */
+
+public interface IDecipher {
+    String decrypt(String encryptedText, String key);
+    String repeat(String encryptedText, String key);
+}
---
+++ src/SealOfV.java
@@ -0,0 +1,22 @@
+/**
+ * Welcome to the final puzzle.
+ * These methods are vague since you're supposed to do research.
+ *
+ * The sentence to decrypt is: Hzol bse, cjc'bi smxyspd wmx wlz kuveitx tzlclmrr! Opgrxw wss aliqmqk.
+ *
+ * The secret to solving this puzzle can be found on the puzzle webpage.
+ *
+ * As usual, try not to use ChatGPT.
+ */
+
+public class SealOfV implements IDecipher{
+
+    public String decrypt(String encryptedText, String key) {
+        return "";
+    }
+
+
+    public String repeat(String encryptedText, String key) {
+        return "";
+    }
+}
